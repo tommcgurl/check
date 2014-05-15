c7191b303e9ae1fec5813106f709556803c2698d
Tom McGurl <Acousticlink@gmail.com>
2014-05-14 22:34:04 -0400

    Initial Commit!
    Adding the basic functionality. This is far from done and needs a ton
    of work but for now it'll be fun to play with.

--- README.md
+++ README.md
@@ -2,3 +2,21 @@ check
 =====
 
 Utility for checking if a nested object is valid.
+
+Experimenting with this for now. It is much in its infancy.
+
+While working for a large company with rich data objects I 
+found myself constantly writing humungous if statements to ensure
+the nested attribute I was accessing was valid. I'm working on this
+as a shothand way to grab a nested object or attribute.
+
+So far the functionality allows for 
+
+	* Getting an object if valid or a fallback value if not valid
+
+	* Finding whether or not an object is valid
+
+	* Getting the evaluation on an array of objects
+
+	* Doing all of this without writing long if statements checking
+	each value along the way
---
+++ check.js
@@ -0,0 +1,82 @@
+/**
+ *  Check.js 1.0.0
+ *  @author TheProfessor117 ( Tom McGurl ) 
+ */
+(function() {
+	check = {};
+
+	var
+	self = this,
+	defFallback = false;
+
+	var _suspectValid = function _suspectValid(suspect) {
+		if((suspect.charAt(0) !== '.' && suspect.charAt(0) !== '[') || !(typeof suspect === 'string')) {
+			return false;
+		} else { 
+			return true;
+		}
+	};
+	
+	/*
+	 *  Check to see if an nested object is defined or not.
+	 *  First argument is an object
+	 *  Second argumnt is a String
+	 *  Third argument is optional
+	 *  Returns the object if valid and either fallback or defFallback
+	 *  if fallback is not passed
+	 */
+	check.use = function(obj, suspect, fallback) {
+		var res = fallback || defFallback;
+		if(!_suspectValid(suspect)){
+			return undefined
+		}
+		try {
+			return eval('obj' + suspect) || res;
+		} catch(e) {
+			return res;
+		}
+	};
+	/*
+	 *  First argument is an object
+	 *  Second argumnt is a String
+	 *  Third argument is optional
+	 *  Returns true if object is valid and fallback if not or false
+	 *  if fallback is not passed
+	 */
+	check.check = function check(obj, suspect, fallback) {
+		var res = fallback || false;
+		if(!_suspectValid(suspect)){
+			return undefined;
+		}
+		try {
+			return !!eval('obj' + suspect) || res;
+		} catch(e) {
+			return res;
+		}
+	};
+	/*
+	 *  First argument is an array of objects
+	 *  Second argumnt is an array of strings
+	 *  Third argument is optional
+	 *  Returns an array of results from check.check for each
+	 *  pair in the provided arrays.
+	 */
+	check.checkList = function check(objArray, suspectArray, fallback) {
+		var resultArray = [];
+		if (!Array.isArray(objArray) || !Array.isArray(objArray)) {
+			return false;
+		}
+		for (var i = objArray.length - 1; i >= 0; i--) {
+			resultArray.push(self.check.use(objArray[i], suspectArray[i], fallback));
+		};
+		return resultArray;
+	}
+
+	/*
+	 * Set the default Fallback
+	 */
+	check.setDefaultFallback = function setDefaultFallback(fallback) {
+		self.defFallback = fallback || self.defFallback;
+	}
+
+})();
